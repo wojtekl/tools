3e830b0be75138ec86499f09227a385e56e0bb94
wojtekel <lesniakwojciech at gmail dot com>
2018-01-31 18:04:10 +0100

    project updated

--- linux/ubuntu-konfiguracja.sh
+++ linux/ubuntu-konfiguracja.sh
@@ -10,12 +10,11 @@ sudo ufw allow out 80/tcp
 sudo ufw allow out 443/tcp
 sudo ufw allow out 1935/tcp
 
-sudo apt-get purge libreoffice-*
 sudo add-apt-repository ppa:libreoffice/ppa
-
 sudo apt-get update
 sudo apt-get autoremove
 sudo apt-get autoclean
+sudo apt-get purge libreoffice-common
 sudo apt-get install linux-generic
 sudo apt-get upgrade
 
@@ -29,15 +28,16 @@ sudo apt-get install myspell-pl
 sudo apt-get install hyphen-pl
 sudo apt-get install mythes-pl
 
-sudo sed -i 's/"quiet splash"/"modprobe.blacklist=serio_raw,lp,ppdev,parport_pc,joydev,pata_acpi quiet splash"/g' /etc/default/grub
-sudo sed -i 's/GRUB_TIMEOUT=.*/GRUB_TIMEOUT=3/g' /etc/default/grub
+sudo sed -i 's/GRUB_TIMEOUT=.*/GRUB_TIMEOUT=2/g' /etc/default/grub
+sudo sed -i 's/"quiet splash"/"modprobe.blacklist=serio_raw,lp,ppdev,parport_pc,joydev,pata_acpi vt.handoff=7 quiet splash"/g' /etc/default/grub
+sudo sed -i 's/#GRUB_GFXMODE=640x480/#GRUB_GFXMODE=640x480\nGRUB_GFXPAYLOAD_LINUX=1280x720/g' /etc/default/grub
 sudo update-grub
 
 sudo sed -i 's/errors=remount-ro/errors=remount-ro,relatime/g' /etc/fstab
 
 sudo sed -i 's/^exit 0/echo US15 | sudo tee \/proc\/acpi\/wakeup\n\nexit 0/' /etc/rc.local
 
-sed -i '1i"synclient TouchpadOff=$(synclient -l | grep -c\n\x27TouchpadOff.*=.*0\x27)"\n    XF86TouchpadToggle\n' .config/peppermint-control-center/xbindkeys.conf
+sudo nvidia-xconfig --no-logo
 
 
 # instalacja oprogramowania jako root
